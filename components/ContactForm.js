4ac360577070ba2841f360fbfadbcae4da7bfa79
Dariusz Rosiek <daro.krk@interia.pl>
2018-03-01 17:59:30 +0100

    Create folder and files, change components files to JSX syntax

---
+++ components/ContactForm.js
@@ -0,0 +1,15 @@
+var ContactForm = React.createClass({
+	propTypes: {
+		contact: React.PropTypes.object.isRequired
+	},
+	render: function() {
+		return (
+			<form className={'contactForm'}>
+				<input type={'text'} placeholder={'Name'} value={this.props.contact.firstName}</input>
+				<input type={'text'} placeholder={'Surname'} value={this.props.contact.lastName}</input>
+				<input type={'email} placeholder={Email'} value={this.props.contact.email}</input>
+				<button type={'submit'}>Add contact</button>
+			</form>
+		);
+	}
+});
